20bc923dbe3602fe95ca7aae415ce2a695d7051d
hollanlu <hollanlu@mail.gvsu.edu>
2016-10-05 06:21:39 -0400

    Add files via upload

--- subCipher.c
+++ subCipher.c
@@ -11,20 +11,59 @@ void processInput(FILE*, FILE*, char[]);
 
 int main(int argc, char* argv[])
 {
-	int i;
-	char* key = NULL;
+	char *rawKey, *key = NULL;
 	FILE *fin = NULL, *fout = NULL;
+	int choice;
+	char ch;
+	char encrypt[26], decrypt[26];
 	
-	key = argv[1];  // get encryption key
-	fin = fopen(argv[2], "r");  // open input file
-	fout = fopen(argv[3], "w");  // open output file
-	
-	char keyArr[strlen(key)];  // array of characters in encryption key
-	
-	for (i = 0; i < strlen(key); i++)  // key into array for functions
+	// check for valid amount of arguments
+	if (argc != 5)
 	{
-		keyArr[i] = key[i] % 26;
+		printf("Whoops, looks like you don't the right arguments.\n");
+		printf("Try again! You need a key, a choice, and two files.\n");
+		exit(1);
+	}	
+	
+	rawKey = argv[1];  // get raw (duplicat letters) encryption key
+	choice = atoi(argv[2]);  // encryption (1)  or decryption (2)?
+	fin = fopen(argv[3], "r");  // open input file
+	fout = fopen(argv[4], "w");  // open output file
+	
+	// check for valid files
+	if (fin == NULL || fout == NULL)
+	{
+		printf("Sorry, you have at least one non-valid file.\n");
+		printf("Please double-check and try again!\n");
+		exit(1);
 	}
+	
+	// convert raw key to key without duplicate letters
+	key = removeDuplicates(rawKey);
+	
+	if (choice == 1)  // encryption selected
+	{
+		initializeEncryptArray(key, encrypt);
+	}
+	else if (choice == 2)  // decryption selected
+	{
+		initializeDecryptArray(key, decrypt);
+	}
+	else  // nothing selected
+	{
+		printf("Sorry, that's not a valid choice!\n");
+		printf("Please try again.\n");
+		printf("Enter 1 for encryption, 2 for decryption.\n");
+	}
+	
+	
+	while (fscanf(fin, "%c", &ch) != EOF)
+	{
+		// encrypt/decrypt
+	}
+	
+	fclose(fin);
+	fclose(fout);
 	
 	return 0;
 }
@@ -32,13 +71,38 @@ int main(int argc, char* argv[])
 // removes duplicate letters from keyword and returns result string
 char * removeDuplicates(char word[])
 {
-	return 0;
+	int i, j = 0;
+	char* result = malloc(strlen(word) * sizeof(char));  // new key
+	char temp[strlen(word)];  // temporary array to hold characters
+	
+	for(i = 0; i < strlen(word); i++)
+	{
+		// if character isn't already in result, add it
+		if (strchr(temp, word[i]) == NULL)
+		{
+			temp[j] = word[i];
+			j++;
+		}
+	}
+	
+	temp[j] = '\0';  // make a string
+	result = temp;  // temp is local, so set result to temp
+	return result;  // return key w/o duplicate letters
 }
 
 // searches for the first num characters in given array
 int targetFound(char charArray[], int num, char target)
 {
-	return 0;
+	int found = 0;
+	char temp[num];
+	
+	strncpy(temp, charArray, num);
+	if (strchr(temp, target) != NULL)
+	{
+		found = strchr(temp, target);
+	}
+	
+	return found;
 }
 
 // initialize encrypt array with cipher letters from given key
@@ -48,7 +112,7 @@ void initializeEncryptArray(char key[], char encrypt[])
 }
 
 // TODO
-void initalizeDecryptArray(char encrypt[], char decrypt[])
+void initializeDecryptArray(char encrypt[], char decrypt[])
 {
 	//
 }
